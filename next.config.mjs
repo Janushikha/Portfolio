8437e2068f7326c9136644fa8de718cf71047c41
Janushikha <gjanushikha.23@uom.lk>
2026-08-13 22:25:35 +0530

    feat:build gh page

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 import withMDX from '@next/mdx'
 
+const basePath = process.env.NEXT_BASE_PATH || '';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    output: 'export',
+    trailingSlash: true,
+    basePath,
+    assetPrefix: basePath,
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default withMDX()(nextConfig)
